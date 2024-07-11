1a68f7936996263edc7d939cbcc05493520e1238
Slade Harker <slade_harker@hotmail.com>
2024-07-12 08:06:25 +1000

    demo-001 setup

---
+++ demo-001/main.py
@@ -0,0 +1,9 @@
+# main.py
+import time
+from machine import Pin
+led=Pin("LED",Pin.OUT)        #create LED object from pin13,Set Pin13 to output
+
+while True:
+  led.toggle()
+  time.sleep(0.5)
+  
